e3b714e91640dd1f854b1ec20d9be83c69318c73
Marcelo Leivas <marceloleivas@Marcelos-MacBook-Pro.local>
2021-07-06 18:58:34 -0300

    locators/base_page docstring to help who use it

--- {{cookiecutter.project_name}}_bdd/support/locators/base_page.py
+++ {{cookiecutter.project_name}}_bdd/support/locators/base_page.py
@@ -8,6 +8,9 @@
         - LINK_TEXT
         - PARTIAL_LINK_TEXT
 
+    Insert locators that are common to all pages.
+    You should do the same for other pages and their unique elements
+
     Example:
         nav_logo = ("ID", "nav-logo-sprites")
 """
