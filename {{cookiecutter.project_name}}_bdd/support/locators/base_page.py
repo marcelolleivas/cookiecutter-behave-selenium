7c798544eea83a2b482739ef609b6086db3f3ecb
Marcelo Leivas <marceloleivas@Marcelos-MacBook-Pro.local>
2021-07-06 15:49:24 -0300

    create base_page.py for instructions

---
+++ {{cookiecutter.project_name}}_bdd/support/locators/base_page.py
@@ -0,0 +1,13 @@
+"""
+    SUPPORTED LOCATORS STRATEGIES:
+        - XPATH
+        - ID
+        - NAME
+        - CSS_SELECTOR
+        - TAG_NAME
+        - LINK_TEXT
+        - PARTIAL_LINK_TEXT
+
+    Example:
+        nav_logo = ("ID", "nav-logo-sprites")
+"""
